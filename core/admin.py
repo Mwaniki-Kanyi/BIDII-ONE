a8e373b5a2f151c676c2363ea821d4a31d890155
THE PENTAGON MOVEMENT. <134065998+KingT5M@users.noreply.github.com>
2025-10-16 01:36:44 +0300

    Registered models and created super user credentials

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Customer, Worker, Estimate, Project, Invoice
+
+admin.site.register(Customer)
+admin.site.register(Worker)
+admin.site.register(Estimate)
+admin.site.register(Project)
+admin.site.register(Invoice)
